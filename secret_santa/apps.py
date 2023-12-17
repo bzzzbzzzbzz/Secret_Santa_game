23ccc65bff03c4d14f611059b6aefe4cf81ecfc6
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-12-17 17:39:13 +0300

    apps.py -
    added verbose_name

--- secret_santa/apps.py
+++ secret_santa/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SecretSantaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'secret_santa'
+    verbose_name = 'Игра "Тайный Санта"'

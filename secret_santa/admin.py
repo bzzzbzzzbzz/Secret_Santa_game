ee699663c8f4e359422238c8fd1bc19059802f22
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-12-17 17:38:14 +0300

    admin.py -
    the admin panel has been developed

--- secret_santa/admin.py
+++ secret_santa/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Game, Patricipants, Givers
-# from .forms import UsersForm
+from .forms import PatricipantsForm, GiversForm
 
 
 @admin.register(Game)
@@ -9,7 +9,9 @@ class GameAdmin(admin.ModelAdmin):
                     'start_of_registration', 'end_of_registration', 'departure_date',
                     'link_to_the_game'
                     )
-    # form = UsersForm
+    search_fields = ('name_of_game', 'start_of_registration', 'cost_of_the_gift')
+    list_editable = ('cost_of_the_gift', 'start_of_registration', 'end_of_registration', 'departure_date')
+    list_filter = ('cost_of_the_gift',)
 
 
 @admin.register(Patricipants)
@@ -17,9 +19,15 @@ class PatricipantsAdmin(admin.ModelAdmin):
     list_display = ('id', 'game', 'id_user', 'name',
                     'e_mail', 'interests', 'letter_to_santa'
                     )
+    search_fields = ('game',)
+    list_filter = ('game',)
+    form = PatricipantsForm
 
 
 @admin.register(Givers)
 class GiversAdmin(admin.ModelAdmin):
     list_display = ('id', 'game', 'givers', 'recipient'
                     )
+    search_fields = ('game',)
+    list_filter = ('game',)
+    form = GiversForm

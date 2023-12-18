80650ca64b2517d045552d5dadff22d4017aef3d
bzzzbzzzbzz <65319137+bzzzbzzzbzz@users.noreply.github.com>
2023-12-18 15:15:00 +0100

    Update settings.py

--- tg_admin/settings.py
+++ tg_admin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["your_app_name.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["santa-test-37f66e3227f0.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
